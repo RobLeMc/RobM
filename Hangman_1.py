0c87f5a4c6d6fb62c420d29551e9d9733968f8bb
RobLeMc <164811814+RobLeMc@users.noreply.github.com>
2024-10-07 23:33:39 -0500

    Add files via upload

---
+++ Hangman_1.py
@@ -0,0 +1,19 @@
+#Make the algorithm page first
+hidden_word = input("What is your hidden word? ")
+guess_letter = input("What is your guess? ")
+#validate guess letter. It needs to first be read in as one character and then checked if its a letter. Make it a while loop
+
+
+def incorrect_guess():
+    pass
+
+def loser():
+    pass
+
+def winner():
+    pass
+
+def check_guess():
+    #write a loop that checks for letter in hidden word
+        pass
+
